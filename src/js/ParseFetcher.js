178e267cb1592e375b3bbc300323d8b0ea756641
devchakraborty <devchakraborty@users.noreply.github.com>
2015-09-12 21:22:40 -0400

    Clean up logs

--- src/js/ParseFetcher.js
+++ src/js/ParseFetcher.js
@@ -11,29 +11,23 @@ let ParseFetcher = {
 				var arrCandidates = [];
 				var arrKeys = ["first_name", "last_name", "party", "objectId"]; 
 				for (var j = results.length - 1; j >= 0; j--) {
-					console.log("inside for loop")
 					var candidate = {}
 					for (var i = 0; i < 4; i++) {
 						switch(arrKeys[i]) { 
 							case "first_name": 
 								candidate["first_name"] = results[j].get("first_name");
-								console.log("inside switch: first_name", results[j].get("first_name"))
 								break;
 							case "last_name": 
 								candidate["last_name"] = results[j].get("last_name");
-								console.log("inside switch: last_name", results[j].get("last_name"))
 								break;
 							case "party": 
 								candidate["party"] = results[j].get("party");
-								console.log("inside switch: party", results[j].get("party"))
 								break;
 							case "objectId": 
 								candidate["id"] = results[j].id;
-								console.log("inside switch: id", results[j].id)
 								break;
 						}
 					}
-					console.log("CANDIDATE MAP", candidate);
 					arrCandidates.push(candidate);
 				};	
 				resolve(arrCandidates)
@@ -58,7 +52,6 @@ let ParseFetcher = {
 		// candidateIDs: array, candidate ids to filter videos by, don't filter if empty array
 		// offset: int, skip the first offset # of elements
 		// limit: int, only return limit # of elements
-		console.log("GET VIDEOS", issueIDs, candidateIDs)
 		return new Promise((resolve, reject) => {
 			setTimeout(() => {
 				let videos = [{"id": "e", "issue_id":"c", "candidate_id":"a", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "f", "issue_id":"c", "candidate_id":"b", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "g", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "h", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "i", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "j", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "k", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "l", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "m", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "n", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "o", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}, {"id": "p", "issue_id":"a", "candidate_id":"c", "video_url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]
